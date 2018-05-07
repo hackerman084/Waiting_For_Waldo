0bdc3a6815555fa5f8a2052aa28e9d4c07549bd0
ehallx <38925749+ehallx@users.noreply.github.com>
2018-05-07 15:45:47 -0400

    Add files via upload

--- waldo_cnn.py
+++ waldo_cnn.py
@@ -1,203 +1,212 @@
-import keras
-import pandas as pd 
-import numpy as np 
-import os
+"""
+Written by Elliot Hall, Astha Singhal and Shruti Sharma
+This code was written for the final project for PHYS476.
+
+====== NOTES ======
+This program will accept multiple folders containing categorical sub-folders, in the case of the final project,
+the sub folders are "waldo" and "not waldo". The program will read in all of those pictures into 2 lists
+the first list contains square pictures in the folder.
+The second list contains an equal number of labels. The label is the name of the folder by default.
+
+All pictures submitted to the training program need to be the same size and square.
+
+The labels are one-hot-encoded
+The program is trained and the Keras model is exported.
+"""
+
+# ====== IMPORTS ======
+import sys              # imports the system for passing command line arguments
+import os               # imports the os stuff
+import time             # imports time to do timing stuff
+import cv2 as cv        # imports OpenCV as cv
+import numpy as np      # imports numpy
+import pandas as pd     # imports pandas for data frame use
 from sklearn.model_selection import train_test_split
+
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-import cv2 
-import random
-# maybe play with making a label array with 2 columns
-path = "/Users/AsthaSinghal/Desktop/Hey-Waldo/64/"
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from keras.utils import np_utils
+from keras.models import load_model
+import tensorflow
 
 
-def color_array(arr, waldo_data, labels): 
-	print("B")
-	print(len(waldo_data))
-	print(len(labels))
-	for i in range(1,20):
-		#print(i)
-		rand = random.randint(1,30)
-		red_copy = arr.copy()
-		blue_copy = arr.copy()
-		green_copy = arr.copy()
-		rg_copy = arr.copy()
-		rb_copy = arr.copy()
-		gb_copy = arr.copy()
-		rgb_copy = arr.copy()
-		for x in range(0,64):
-			for y in range(0,64):
-				red_copy.itemset((x,y,2), rand)
-				blue_copy.itemset((x,y,0), rand)
-				green_copy.itemset((x,y,1), rand)
-				rg_copy.itemset((x,y,2), rand)
-				rg_copy.itemset((x,y,1), rand)	
-				rb_copy.itemset((x,y,2), rand)
-				rb_copy.itemset((x,y,0), rand)
-				gb_copy.itemset((x,y,1), rand)
-				gb_copy.itemset((x,y,0), rand)
-				rgb_copy.itemset((x,y,0),rand)
-				rgb_copy.itemset((x,y,1),rand)
-				rgb_copy.itemset((x,y,2),rand)
-		waldo_data.append(red_copy)
-		waldo_data.append(blue_copy)
-		waldo_data.append(green_copy)
-		waldo_data.append(rg_copy)
-		waldo_data.append(rb_copy)
-		waldo_data.append(gb_copy)
-		waldo_data.append(rgb_copy)
-		labels = labels + [1,1,1,1,1,1,1]
-	print("E")
-	print(len(waldo_data))
-	print(len(labels))
-	return labels
-#importing all of the waldo files
+# ----- DATA PATH: -----
+# gives the path of the location this file is running in
+file_path = os.path.dirname(os.path.realpath('__file__'))
 
-import random
-import numpy as np
+# the argument passed is the folder containing the images.
+# the folder passed must be a direct offspring of the folder containing this piece of code.
 
-WINDOW_SIZE = 64
+# All appending will be done in commutable forms,
+# This is to prevent copying of the entire set multiple times and slowing down the process.
 
-def get_best_bounding_box(image, model, step=10):
-	img = cv2.imread(image)
-	#initializing vars
-	best_box = None
-	best_box_prob = -np.inf
+# Empty list that will fill up with the image arrays
+x_data_set = []
 
-	# loop window sizes: 20x20, 30x30, 40x40...160x160
-	for top in range(0, img.shape[0] - WINDOW_SIZE + 1, step):
-		for left in range(0, img.shape[1] - WINDOW_SIZE + 1, step):
-		# compute the (top, left, bottom, right) of the bounding box
-			box = (top, left, top + WINDOW_SIZE, left + WINDOW_SIZE)
-			# crop the original image
-			cropped_img = img[box[0]:box[2], box[1]:box[3]]
-			# predict how likely this cropped image is dog and if higher
-			# than best save it
-			print('predicting for box %r' % (box, ))
-			cropped_img = np.reshape(cropped_img, (1,64,64,3))
-			box_prob = predict_function(model, cropped_img)
-			print(str(box_prob))
-			if box_prob > best_box_prob:
-				best_box = box
-				best_box_prob = box_prob
+# Empty list that will fill up with the folder names
+y_data_set = []
 
-	return best_box
+# ====== DATA IMPORTATION ======
 
-def predict_function(model, x):
-	return model.predict(x)
+# The Following nested for loops are iterating through all of the data in all of the folders and creating a numpy array
+# That will contain each picture and its label.
+file_count = 0
+index = 1
+# For every folder passed in as an argument
+for index in range(1, len(sys.argv)):
+    image_folder_path = sys.argv[index]
 
+    # For every sub-folder in the folder passed
+    for folder_name in os.listdir(image_folder_path):
+        image_type_folder_path = os.path.join(image_folder_path, folder_name)
 
-# # dummy array of 256X256
-# img = np.arange(256 * 256).reshape((256, 256))
+        print('-------------------------------', folder_name, '-------------------------------')
 
-# best_box = get_best_bounding_box(img, predict_function)
-# print('best bounding box %r' % (best_box, ))
+        # For each file in the folder:
+        for filename in os.listdir(image_type_folder_path):
 
+            # The file extension that will be used to check the type of file it is.
+            temp_file_ext = os.path.splitext(filename)[1]
 
-def preprocessing(path, waldo_data, labels):
-	for image in os.listdir(path + "waldo"): 
-		#print(image + " W")
-		arr = cv2.imread(path + "waldo/" + image)
-		horiz_img = arr.copy()
-		vert_img = arr.copy()
-		both_img = arr.copy()
-		
-		horiz_img = cv2.flip(horiz_img,0)
-		vert_img = cv2.flip(vert_img,1)
-		both = cv2.flip(both_img, -1)
-		h_copy = horiz_img.copy()
-		v_copy = vert_img.copy()
-		b_copy = both.copy()
+            # If the file is a jpg file extension then we will use it,
+            # this just gets any random file types out of the way.
+            if temp_file_ext == '.jpg' or ".jpeg":
+                file_count = file_count + 1
 
-		waldo_data.append(arr)
-		waldo_data.append(horiz_img)
-		waldo_data.append(vert_img)
-		waldo_data.append(both)
-		labels = labels + [1,1,1,1]
+                # the location of each file as used by the OpenCV reading method.
+                image_path = os.path.join(image_type_folder_path, filename)
+                print('%5d' % file_count, '\t', image_path, "  \t", end="")
 
-		labels = color_array(arr, waldo_data, labels)
+                # reads the file as a numpy array in the form of a matrix with the
+                image = cv.imread(image_path)
+                image_dim, _, _ = image.shape
+                print(image_dim)
 
-	#importing all of the non_waldo images 
-	non_waldo = []
-	non_waldo_labels = []
-	for image in os.listdir(path + "notwaldo"): 
-		#print(image + " NW")
-		arr = cv2.imread(path + "notwaldo/" + image)
-		#waldo_data.append(arr)
-		non_waldo.append(arr)
-		non_waldo_labels.append(0)
+                # print(np.shape(image), '\n')
 
-	return waldo_data, labels, non_waldo, non_waldo_labels
+                # Appending the original images to the x and y datasets
+                x_data_set.append(image)
+                y_data_set.append(folder_name)
 
+                # ----- IMAGE PROCESSING: -----
+                # We do not have enough images of waldo off the bat so
+                # we need to make more using various transformations.
+                if folder_name == 'waldo':
 
-def create_model():
-	model = Sequential()
-	model.add(Conv2D(32, (3, 3), input_shape=(64,64,3)))
-	model.add(Activation('relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
+                    # Rotating +90, +180, +270
+                    r = 1
+                    print("\t\t", end="")
+                    while r < 3:
+                        image_R = np.rot90(image, k=r, axes=(0, 1))
+                        x_data_set.append(image_R)
+                        y_data_set.append(folder_name)
+                        print(r, end=" ")
+                        r = r + 1
+                        file_count = file_count + 1
 
-	model.add(Conv2D(32, (3, 3)))
-	model.add(Activation('relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
+                    # Flipping Horizontally
+                    image_H = np.flip(image, axis=0)
+                    x_data_set.append(image_H)
+                    y_data_set.append(folder_name)
+                    print(4, end=" ")
+                    file_count = file_count + 1
 
-	model.add(Conv2D(64, (3, 3)))
-	model.add(Activation('relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
+                    # Rotating the Flipped image +90
+                    image_H_90 = np.rot90(image_H, k=1, axes=(0, 1))
+                    x_data_set.append(image_H_90)
+                    y_data_set.append(folder_name)
+                    print(5, end=" ")
+                    file_count = file_count + 1
 
-	model.add(Flatten())
-	model.add(Dense(64))
-	model.add(Activation('relu'))
-	model.add(Dropout(0.15))
-	model.add(Dense(1))
-	model.add(Activation('softmax'))
+                    # Flipping Vertically
+                    image_V = np.flip(image, axis=1)
+                    x_data_set.append(image_V)
+                    y_data_set.append(folder_name)
+                    print(6, end=" ")
+                    file_count = file_count + 1
 
-	model.compile(loss='binary_crossentropy',
-				  optimizer='adam',
-				  metrics=['accuracy'])
-	return model
+                    # Rotating the Flipped image +90
+                    image_V_90 = np.rot90(image_V, k=1, axes=(0, 1))
+                    x_data_set.append(image_V_90)
+                    y_data_set.append(folder_name)
+                    print(7)
+                    file_count = file_count + 1
+
+            else:
+                continue
+
+# Casting the lists as numpy arrays
+x_data_set = np.asarray(x_data_set)
+y_data_set = np.asarray(y_data_set)
+
+# Doing one-hot-encoding on the labels.
+y_data_set = pd.get_dummies(y_data_set)
+
+print("\n")
+print("X Data set: ", len(x_data_set), "entries.\tShape: ", x_data_set.shape)
+print("Y Data set: ", len(y_data_set), "entries.\tShape: ", y_data_set.shape)
+print("\n")
+print("---------------------------     FINISHED IMPORTING DATA     ---------------------------\n\n")
+
+# ====== SPLITTING INTO TRAINING AND TESTING DATA ======
+x_train, x_test, y_train, y_test = train_test_split(x_data_set, y_data_set, test_size=0.10, random_state=42)
 
 
-
-def train_model(waldo_data, labels, non_waldo, non_waldo_labels, model):
-	x_trainW, x_testW, y_trainW, y_testW = train_test_split(waldo_data,labels, test_size=0.2, random_state=42)
-	x_trainNW, x_testNW, y_trainNW, y_testNW = train_test_split(non_waldo,non_waldo_labels, test_size=0.2, random_state=42)
-
-	x_train = x_trainW + x_trainNW
-	x_test = x_testW + x_testNW
-	y_train = y_trainW + y_trainNW
-	y_test = y_testW + y_testNW
-
-	x_train = np.array(x_train)
-	x_test = np.array(x_test)
-	y_train = np.array(y_train)
-	y_test = np.array(y_test)
-
-	print(x_train.shape)
-	print(x_test.shape)
-	print(y_train.shape)
-	print(y_test.shape)
-
-	model.fit(x_train, y_train,epochs=1,batch_size=200)
-	scores = model.evaluate(x_test,y_test)
-	print("\n%s: %.2f%%" %(model.metrics_names[1], scores[1]))
-	return model 
+np.random.seed(5)
+show_data = 1                   # Variable used to suppress output, 1 = show, 0 = don't show anything
+training_time = time.time()        # Starting timestamp, only used to determine how long the program will run
 
 
-waldo_data = []
-labels = []
+# ====== CREATING THE MODEL ======
+model = Sequential()
 
-waldo_dataA, labelsA, non_waldoA, non_waldo_labelsA = preprocessing("/Users/AsthaSinghal/Desktop/Hey-Waldo/64/", waldo_data, labels)
-model = create_model()
-train_model(waldo_dataA, labelsA, non_waldoA, non_waldo_labelsA, model)
+# Adds a Convolution layer to the model,
+model.add(Conv2D(32, (4, 4), padding='same', data_format='channels_first', activation='relu',
+                 input_shape=(image_dim, image_dim, 3)))
 
-get_best_bounding_box("/Users/AsthaSinghal/Desktop/Hey-Waldo/original-images/1.jpg", model)
+model.add(Conv2D(32, (2, 2), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
 
+# Adding a Dense layer to the model with 100 nodes
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.5))
 
+# This is the last layer of the model and because there are only 2 options:
+# "Waldo" and "Not Waldo" there should only be 2 nodes here.
+model.add(Dense(2, activation='softmax'))
 
+# ====== HYPER-PARAMETERS ======
+batch_size = 100
+epochs = 2          # Easy way to change the number of epochs
 
+# ====== COMPILES THE MODEL ======
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy']
+              )
 
+model.fit(x_train,
+          y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=show_data,
+          validation_split=0.40,
+          shuffle=True,
+          # validation_data=(data_file_test, data_labels_test)
+          )
 
+# ====== EVALUATING THE MODEL ======
+scores = model.evaluate(x_test, y_test,
+                        verbose=show_data
+                        )
 
+# ====== PRINTING ACCURACY ======
+print('%s: %0.6f%%' % (model.metrics_names[1], scores[1]*100))
 
+# ====== SAVING THE KERAS MODEL ======
+model.save('waldo_model' + str(image_dim) + '.h5')
+
+# ====== END OF PROGRAM ======
 
